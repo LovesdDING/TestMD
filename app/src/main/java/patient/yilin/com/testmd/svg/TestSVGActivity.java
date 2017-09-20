f36e7f0ae69aa7b0891644b8ffc2899180d5c939
LovesdDING <451972500@qq.com>
2017-09-20 17:30:13 +0800

    commit 6 SVG 动画2

--- app/src/main/java/patient/yilin/com/testmd/svg/TestSVGActivity.java
+++ app/src/main/java/patient/yilin/com/testmd/svg/TestSVGActivity.java
@@ -1,7 +1,11 @@
 package patient.yilin.com.testmd.svg;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import patient.yilin.com.testmd.R;
 
@@ -14,4 +18,11 @@ public class TestSVGActivity extends AppCompatActivity {
     }
 
 
+    public void animStar(View view) {
+        ImageView iv = (ImageView) view;
+        Drawable drawable = iv.getDrawable() ;
+        if(drawable instanceof Animatable){
+            ((Animatable) drawable).start();   //drawable  的启动
+        }
+    }
 }
